04b5d3f2d3af5f0f8d57d8504ed0ed4d69c50e22
Jack <winterschladen.j@gmail.com>
2022-03-28 11:33:31 +0100

    Initial passing feature & unit test

--- spec/features/feature_tests_spec.rb
+++ spec/features/feature_tests_spec.rb
@@ -1,11 +1,11 @@
-require 'account'
+describe "Feature-tests" do 
 
-describe Account do
-  describe "#deposit" do
+  describe "I'd like to be able to make a deposit to my account" do 
     it "deposits money to the account" do
       account = Account.new
       account.deposit(100)
       expect(account.balance).to eq 100
     end
   end
+
 end
