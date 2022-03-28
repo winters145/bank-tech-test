67bd9a812ded9dc772fdf8c5152b839518159c29
Jack <winterschladen.j@gmail.com>
2022-03-28 11:37:40 +0100

    Can withdraw money from the account

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,6 +1,7 @@
 require 'account'
 
 describe Account do
+
   describe "#deposit" do
     it "deposits money to the account" do
       account = Account.new
@@ -8,4 +9,14 @@ describe Account do
       expect(account.balance).to eq 100
     end
   end
+
+  describe "#withdraw" do
+    it "withdraws money from the account" do
+      account = Account.new
+      account.deposit(100)
+      account.withdraw(20)
+      expect(account.balance).to eq 80
+    end
+  end
+
 end
--- spec/features/feature_tests_spec.rb
+++ spec/features/feature_tests_spec.rb
@@ -1,10 +1,19 @@
 describe "Feature-tests" do 
 
   describe "I'd like to be able to make a deposit to my account" do 
-    it "deposits money to the account" do
+    it "adds money to the balance of the account" do
       account = Account.new
       account.deposit(100)
       expect(account.balance).to eq 100
+    end
+  end
+
+  describe "I'd like to be able to make a withdrawal from my account" do
+    it "deducts money from the balance of the account" do
+      account = Account.new
+      account.deposit(100)
+      account.withdraw(20)
+      expect(account.balance).to eq 80
     end
   end
 
