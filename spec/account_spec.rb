60063055487d0e8b295bc35a26c4540286c79ba0
Jack <winterschladen.j@gmail.com>
2022-03-31 10:45:10 +0100

    Added line-spacing between tests to improve visibility

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,54 +4,67 @@ require 'account'
 require 'date'
 
 describe Account do
+  
   let(:account) { described_class.new }
   let(:date_today) { Date.today.strftime('%d/%m/%Y') }
 
   describe '#deposit' do
+
     it 'adds money to the account balance' do
       account.deposit(100.00)
       expect(account.balance).to eq 100.00
     end
+
     it 'throws an error if input is not an integer' do
       expect { account.deposit('fifty') }.to raise_error 'Invalid input: please enter a number'
     end
+
   end
 
   describe '#withdraw' do
+
     it 'deducts money from the account balance' do
       account.deposit(100.00)
       account.withdraw(20.00)
       expect(account.balance).to eq 80.00
     end
+
     it 'throws an error if input is not an integer' do
       expect { account.withdraw('fifty') }.to raise_error 'Invalid input: please enter a number'
     end
+
   end
 
   describe 'transactions' do
+
     it 'records the date that a deposit was made' do
       account.deposit(100.00)
       expect(account.transactions[0][:date]).to eq date_today
     end
+
     it 'records the date that a withdrawal was made' do
       account.deposit(100.00)
       account.withdraw(20.00)
       expect(account.transactions[1][:date]).to eq date_today
     end
+
     it 'records the amount that was deposited' do
       account.deposit(200.00)
       expect(account.transactions[0][:amount]).to eq '200.00'
       expect(account.transactions[0][:type]).to eq :deposit
     end
+
     it 'records the amount that was withdrawn' do
       account.deposit(200.00)
       account.withdraw(100.00)
       expect(account.transactions[1][:type]).to eq :withdrawal
       expect(account.transactions[1][:amount]).to eq '100.00'
     end
+
   end
 
   describe '#print_statement' do
+
     it 'prints a list of the accounts transactions' do
       account.deposit(1000)
       account.withdraw(200)
@@ -63,6 +76,7 @@ describe Account do
           "#{date_today} || 1000.00 ||  || 1000.00\n"
         ).to_stdout
     end
+
   end
 
 end
--- spec/features/feature_tests_spec.rb
+++ spec/features/feature_tests_spec.rb
@@ -3,24 +3,30 @@
 require 'date'
 
 describe 'Feature-tests' do
+
   describe "I'd like to be able to make a deposit to my account" do
+
     it 'adds money to the balance of the account' do
       account = Account.new
       account.deposit(100)
       expect(account.balance).to eq 100
     end
+
   end
 
   describe "I'd like to be able to make a withdrawal from my account" do
+
     it 'deducts money from the balance of the account' do
       account = Account.new
       account.deposit(100)
       account.withdraw(20)
       expect(account.balance).to eq 80
     end
+
   end
 
   describe "I'd like to keep track of my transactions" do
+
     it 'prints a list of all transactions on the account' do
       account = Account.new
       date = Date.today.strftime('%d/%m/%Y')
@@ -34,5 +40,7 @@ describe 'Feature-tests' do
         "#{date} || 1000.00 ||  || 1000.00\n"
       ).to_stdout
     end
+    
   end
+
 end
